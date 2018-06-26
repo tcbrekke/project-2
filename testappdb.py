1bfcec2beb6986b6356c14e39fd35560b1869a56
Thomas Brekke <tcbrekke@gmail.com>
2018-06-25 18:01:39 -0700

    adding functional(?) postgres manipulation code

---
+++ testappdb.py
@@ -0,0 +1,14 @@
+import psycopg2
+import pandas as pd
+import os
+import sqlalchemy
+
+DATABASE_URL = os.environ['DATABASE_URL']
+
+city_sfr_csv = os.path.join("Resources", "City_Zhvi_SingleFamilyResidence(main).csv")
+
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+
+city_sfr_df = pd.read_csv(city_sfr_csv)
+
+city_sfr_df.to_sql('city_sfr.test', conn, if_exists = 'replace', index=False)
