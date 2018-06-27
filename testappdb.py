56099e2ef73b400e67bb094e32da27ac56d3a1a9
Thomas Brekke <tcbrekke@gmail.com>
2018-06-27 07:34:25 -0700

    small change:

--- testappdb.py
+++ testappdb.py
@@ -12,5 +12,5 @@ conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 city_sfr_df = pd.read_csv(city_sfr_csv)
 city_sfr_df.head()
 
-city_sfr_df.to_sql('city_sfr.sqlite', conn, if_exists = 'replace')
+city_sfr_df.to_sql('city_sfr', conn, if_exists = 'replace')
 pd.read_sql('city_sfr.sql').head()
