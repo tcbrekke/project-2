e43166e228fc774fa6b5cccb5e369faea3ba8452
Jessica Burgos <32079816+jessBURGOS@users.noreply.github.com>
2018-06-27 16:50:08 -0700

    zipcode map and geojson

---
+++ logic_zipcode.js
@@ -0,0 +1,20 @@
+// Creating map object
+var map = L.map("map", {
+  center: [37.752361, -122.080389],
+  zoom: 9
+});
+
+// Adding tile layer
+L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?" +
+  "access_token=pk.eyJ1IjoiamVzc2J1cmdvcyIsImEiOiJjamlkdm14dzEwNnZqM3BwbnA3a3ZmcXZ4In0.S_8R6Xa8awTcuIXPU7jexA").addTo(map);
+
+// var link = "https://data.sfgov.org/api/views/f9wk-m4qb/rows.json?accessType=DOWNLOAD";
+// var link = "BayAreaCounties.geojson"
+// Grabbing our GeoJSON data..
+d3.json('zipcodes.geojson.json', function(data) {
+  // Creating a GeoJSON layer with the retrieved data
+  L.geoJson(data).addTo(map);
+});
+
+// var geojsonLayer = new L.GeoJSON.AJAX("BayAreaCounties.geojson");       
+// geojsonLayer.addTo(map);
