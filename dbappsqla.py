807af627dee8df6041bf8dc814e4340f3aa85637
Thomas Brekke <tcbrekke@gmail.com>
2018-06-27 19:41:53 -0700

    Updated to include all cleaned data and method for running

--- dbappsqla.py
+++ dbappsqla.py
@@ -9,12 +9,19 @@ database = "project2scratch"
 
 DATABASE_URL = os.environ['DATABASE_URL']
 
-city_sfr_csv = os.path.join("Resources", "City_Zhvi_SingleFamilyResidence(main)CLEAN.csv")
+region_affordability_csv = os.path.join("Resources", "Affordability_Wide_2018Q1_PublicCLEAN.csv")
+city_single_family_csv = os.path.join("Resources", "City_Zhvi_SingleFamilyResidence(main)CLEAN.csv")
+county_single_family_csv = os.path.join("Resources", "County_Zhvi_SingleFamilyResidenceCLEAN.csv")
+county_multi_family_csv = os.path.join("Resources", "County_Zri_AllHomesPlusMultifamilyCLEAN.csv")
 
 engine = create_engine(DATABASE_URL)
 
-city_sfr_df = pd.read_csv(city_sfr_csv)
+region_affordability_df = pd.read_csv(region_affordability_csv)
+city_single_family_df = pd.read_csv(city_single_family_csv)
+county_single_family_df = pd.read_csv(county_single_family_csv)
+county_multi_family_df = pd.read_csv(county_multi_family_csv)
 
-city_sfr_df.to_sql('city_sfr', engine, if_exists = 'replace')
-pd.read_sql('city_sfr', engine)
-print(engine.table_names())
+region_affordability_df.to_sql('region_affordibility', engine, if_exists = 'replace')
+city_single_family_df.to_sql('city_single_family', engine, if_exists = 'replace')
+county_single_family_df.to_sql('county_single_family', engine, if_exists = 'replace')
+county_multi_family_df.to_sql('county_multi_family', engine, if_exists = 'replace')
