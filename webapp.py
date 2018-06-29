9e5c62c4b6288d8f57c939855c57a304ea6f2bae
Thomas Brekke <tcbrekke@gmail.com>
2018-06-29 14:43:25 -0700

    change database URI

--- webapp.py
+++ webapp.py
@@ -13,12 +13,12 @@ from flask import Flask, render_template, jsonify, request, redirect
 
 from flask_sqlalchemy import SQLAlchemy
 
-# DATABASE_URL = os.environ['DATABASE_URL']
+DATABASE_URL = os.environ['DATABASE_URL']
 
 app = Flask(__name__)
 
 db = SQLAlchemy(app)
-engine = create_engine("postgres://dfpdtekrylpjsy:f81dc88e3ae281c5952015a8cf9af3f4af3d20c05130917c07b80c2197a449b4@ec2-54-225-76-243.compute-1.amazonaws.com:5432/df7r9o4t55h9oc")
+engine = create_engine(DATABASE_URL)
 connection = engine.connect()
 
 Base = automap_base()
