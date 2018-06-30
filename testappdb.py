6a78442ab3bee105b4edf05e06d17ce77c072e6b
Thomas Brekke <tcbrekke@gmail.com>
2018-06-30 10:49:17 -0700

    bring up to date

---
+++ testappdb.py
@@ -0,0 +1,32 @@
+import psycopg2
+import pandas as pd
+import os
+from sqlalchemy import create_engine
+# DATABASE_URL = os.environ['DATABASE_URL']
+
+DATABASE_URL = f"postgresql://tcbrekke:password@localhost/project2scratch"
+
+region_affordability_csv = os.path.join("Resources", "Affordability_Wide_2018Q1_PublicCLEAN.csv")
+city_single_family_csv = os.path.join("Resources", "City_Zhvi_SingleFamilyResidence(main)CLEAN.csv")
+county_single_family_csv = os.path.join("Resources", "County_Zhvi_SingleFamilyResidenceCLEAN.csv")
+county_multi_family_csv = os.path.join("Resources", "County_Zri_AllHomesPlusMultifamilyCLEAN.csv")
+
+engine = create_engine(DATABASE_URL)
+connection = engine.connect()
+
+region_affordability_df = pd.read_csv(region_affordability_csv)
+city_single_family_df = pd.read_csv(city_single_family_csv)
+county_single_family_df = pd.read_csv(county_single_family_csv)
+county_multi_family_df = pd.read_csv(county_multi_family_csv)
+
+region_affordability_df.to_sql('region_affordability', engine, if_exists = 'replace')
+connection.execute("ALTER TABLE region_affordability ADD PRIMARY KEY (Index);")
+
+city_single_family_df.to_sql('city_single_family', engine, if_exists = 'replace')
+connection.execute("ALTER TABLE city_single_family ADD PRIMARY KEY (Index);")
+
+county_single_family_df.to_sql('county_single_family', engine, if_exists = 'replace')
+connection.execute("ALTER TABLE county_single_family ADD PRIMARY KEY (Index);")
+
+county_multi_family_df.to_sql('county_multi_family', engine, if_exists = 'replace')
+connection.execute("ALTER TABLE county_multi_family ADD PRIMARY KEY (Index);")
